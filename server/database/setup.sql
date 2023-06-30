f6950d6669e2d4d131e824ed72f2adb78922f760
anthonychan1211 <anthonychan1211@gmail.com>
2023-06-30 10:10:38 +0100

    edited the users table and diary_entries

--- server/database/setup.sql
+++ server/database/setup.sql
@@ -4,11 +4,16 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE diary_entries (
     entry_id INT GENERATED ALWAYS AS IDENTITY,
-    text VARCHAR (200) NOT NULL,
+    content VARCHAR NOT NULL,
     category VARCHAR (50) NOT NULL,
-    date_time_entry TIMESTAMPTZ NOT NULL,
+    date_time_entry TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ,
     PRIMARY KEY (diary_id)
 );
+
+INSERT INTO diary_entries (content, category) VALUES 
+('This is a good day', 'diary'),
+('This is a bad day','diary'),
+('This is a note.','note')
 
 CREATE TABLE users(
     user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -16,11 +21,11 @@ CREATE TABLE users(
     pass_word VARCHAR(100) NOT NULL
 );
 
-INSERT INTO users(username, pass_word, isAdmin)
+INSERT INTO users(username, pass_word)
 VALUES
-    ('Achan15','$2y$10$097V6YXFoRutj1O42zr1XO2gRXOowekCFpzDewuCvQn8KRrbxVAhO',true),
-    ('Salim1704', '$2y$10$kPwQ5gxsZU3m6K8qptJLA.N0yPT9olUWsKTTZdIElTXkTvwDN1Fly', true),
-    ('Msamin000', '$2y$10$oPHQ4euAqhl6GpmjQ2To1eQXxAnn4upjLT4t9P0sZbRdo2AoEf2B2', false);
+    ('Achan15','$2y$10$097V6YXFoRutj1O42zr1XO2gRXOowekCFpzDewuCvQn8KRrbxVAhO'),
+    ('Salim1704', '$2y$10$kPwQ5gxsZU3m6K8qptJLA.N0yPT9olUWsKTTZdIElTXkTvwDN1Fly'),
+    ('Msamin000', '$2y$10$oPHQ4euAqhl6GpmjQ2To1eQXxAnn4upjLT4t9P0sZbRdo2AoEf2B2');
 
 CREATE TABLE token (
     token_id INT GENERATED ALWAYS AS IDENTITY,
